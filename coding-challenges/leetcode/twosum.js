26ac154d233451be8e5fbeaf151b9ccab158df4d
brandonfourie <Brandonfourie123@gmail.com>
2026-03-06 08:29:14 +0200

    [style] code comments

--- coding-challenges/leetcode/twosum.js
+++ coding-challenges/leetcode/twosum.js
@@ -3,9 +3,13 @@
 // Solved for algorithm practice
 
 function twoSum(nums, target) {
+    // object to store values weve already iterated through
+    // key - nums value
+    // value - index value of key
     let seen = {};
 
     for (let idx = 0; idx < nums.length; idx++){
+        // determie if complement is in seen
         if ((target - nums[idx]) in seen){
             return [idx, seen[target-nums[idx]]];
         }
@@ -15,7 +19,3 @@ function twoSum(nums, target) {
 
 
 console.log(twoSum([2,1,4,3], 7))
-
-// What happens when you assign to a variable without declaring it - it creates an implicit global variable
-
-// return values with commas - javascript executes left to right then discards to the left and only returns the right most return value
